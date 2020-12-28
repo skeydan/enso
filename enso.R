e6e66177a650c3d3d414d6f7d53a862015bfc3c3
skeydan <keydana@gmx.de>
2020-12-28 21:52:19 +0100

    wip

--- enso.R
+++ enso.R
@@ -3,8 +3,6 @@
 # The east-west grid points run eastward from 0.5ºE to 0.5ºW,
 # while the north-south grid points run northward from 89.5ºS to 89.5ºN.
 
-# use 0°–360° E and 55° S–60° N only?
-
 # https://ds.data.jma.go.jp/tcc/tcc/library/MRCS_SV12/index_e.htm
 
 # https://www.climatechange.ai/CameraReadySubmissions%202-119/119/CameraReadySubmission/Forecasting_El_Nino_with_Convolutional_and_Recurrent_Neural_Networks(1).pdf
@@ -15,14 +13,14 @@
 # 
 # https://meso.nju.edu.cn/njdx/DFS//file/2019/11/26/201911261121480744epw3l.pdf?iid=6240
 
-
+library(torch)
 library(purrr)
 library(stars)
 library(readr)
 library(dplyr)
 library(ggplot2)
 library(viridis)
-library(ggthemes)
+#library(ggthemes)
 
 #purrr::walk(readLines("files"), function(f) download.file(url = f, destfile = basename(f)))
 #read_stars("grb/sst189101.grb")
@@ -50,16 +48,21 @@ grb_train <- grb %>% filter(time < as.Date("2000-01-01"))
 grb_train <- as.tbl_cube.stars(grb_train)$mets[[1]] 
 grb_train <- grb_train + 273.15
 quantile(grb_train, na.rm = TRUE)
+train_mean <- mean(grb_train, na.rm = TRUE)
+train_sd <- sd(grb_train, na.rm = TRUE)
+grb_train <- (grb_train - train_mean) / train_sd
 grb_train[is.na(grb_train)] <- 0
+quantile(grb_train, na.rm = TRUE)
   
 grb_valid <- grb %>% filter(time >= as.Date("2000-01-01"))
 grb_valid <- as.tbl_cube.stars(grb_valid)$mets[[1]] 
 grb_valid <- grb_valid + 273.15
+grb_valid <- (grb_valid - train_mean) / train_sd
 quantile(grb_valid, na.rm = TRUE)
 grb_valid[is.na(grb_valid)] <- 0
 
 
-nino <- read_table("ONI_NINO34_1854-2020.txt", skip = 9) %>%
+nino <- read_table2("ONI_NINO34_1854-2020.txt", skip = 9) %>%
   mutate(month = as.Date(paste0(YEAR, "-", `MON/MMM`, "-01"))) %>%
   select(month, NINO34_MEAN, PHASE) %>%
   filter(between(month, as.Date("1891-01-01"), as.Date("2020-09-01")))
@@ -70,34 +73,34 @@ nino_valid <- nino %>% filter(month >= as.Date("2000-01-01"))
 
 #ggplot(nino %>% filter(PHASE != "M"), aes(x = month, y = NINO34_MEAN, color = PHASE)) + geom_path(size = 0.5)
 
-tmp <- as.tbl_cube.stars(grb_valid)$mets[[1]][ , , 1]
-
-library(torch)
+grb_valid %>% dim()
+nino_valid %>% dim()
 
 n_timesteps <- 6
+batch_size <- 16
 
 enso_dataset <- dataset(
   
   name = "enso_dataset",
   
-  initialize = function(ssts, nino34, n_timesteps) {
-   self$x <- as.tbl_cube.stars(ssts)$mets[[1]] 
+  initialize = function(sst, nino34, n_timesteps) {
+   self$x <- sst
    self$y <- nino34
    self$n_timesteps <- n_timesteps
   },
   
   .getitem = function(i) {
-    x <- torch_tensor(self$x[ , , i:(n_timesteps + i - 1)])
-    x <- torch_split(x, 1, dim = 3)
-    x <- torch_stack(x)
+    x <- torch_tensor(self$x[ , , i:(n_timesteps + i - 1)]) # (360, 180, n_timesteps)
+    x <- torch_split(x, 1, dim = 3) # list of length n_timesteps of tensors (360, 180, 1)
+    x <- torch_stack(x) # (n_timesteps, 360, 180, 1)
     x <- x$view(c(n_timesteps, 1, 360, 180))
     
-    y <- torch_tensor(self$y$NINO34_MEAN[i + n_timesteps - 1])
+    y <- torch_tensor(self$y$NINO34_MEAN[(i + 1):(n_timesteps + i)])
     list(x = x, y = y)
   },
   
   .length = function() {
-    nrow(self$y) - n_timesteps + 1
+    nrow(self$y) - n_timesteps 
   }
   
 )
@@ -108,31 +111,140 @@ first <- valid_ds$.getitem(1)
 first$x
 first$y
 
-batch_size <- 8
 valid_dl <- valid_ds %>% dataloader(batch_size = batch_size)
 length(valid_dl)
 
 iter <- valid_dl$.iter()
-b <- iter$.next()
-b
+first_batch <- iter$.next()
+#first_batch
 
 train_ds <-enso_dataset(grb_train, nino_train, n_timesteps)
-train_ds$.length()
-train_dl <- train_ds %>% dataloader(batch_size = 8, shuffle = TRUE)
-train_dl$.length()
+length(train_ds)
+train_dl <- train_ds %>% dataloader(batch_size = batch_size, shuffle = TRUE)
+length(train_dl)
 
 
 # Model -------------------------------------------------------------------
 
 source("../convlstm/convlstm.R")
 
-model <- convlstm(input_dim = 1, hidden_dims = c(5, 1), kernel_sizes = rep(3, 2), n_layers = 2)
-c(layer_outputs, layer_last_states) %<-% model(b$x)
+model <- nn_module(
+  
+  initialize = function(channels_in,
+                        convlstm_hidden,
+                        convlstm_kernel,
+                        convlstm_layers) {
+    
+    self$n_layers <- convlstm_layers
+    self$convlstm <- convlstm(input_dim = channels_in,
+                              hidden_dims = convlstm_hidden,
+                              kernel_sizes = convlstm_kernel,
+                              n_layers = convlstm_layers
+                              )
+    self$output <- nn_linear(360 * 180, 1)
+    
+  },
+  
+  forward = function(x) {
+    
+    c(layer_outputs, layer_last_states) %<-% self$convlstm(x)
+    flat <- torch_flatten(layer_outputs[[self$n_layers]], start_dim = 3)$squeeze(2)
+    self$output(flat)$squeeze(3)
+    
+  }
+    
+)
+
+device <- torch_device(if(cuda_is_available()) "cuda" else "cpu")
+device <- "cpu"
+
+net <- model(channels_in = 1,
+             convlstm_hidden = c(16, 1),
+             convlstm_kernel = c(3, 1),
+             convlstm_layers = 2)
+
+net <- net$to(device = device)
+
+
+# train -------------------------------------------------------------------
+
+optimizer <- optim_adam(net$parameters, lr = 0.001)
+
+num_epochs <- 20
+
+train_batch <- function(b) {
+  
+  print(i)
+  
+  optimizer$zero_grad()
+  output <- net(b$x$to(device = device))
+  target <- b$y$to(device = device)
+  
+  loss <-  nnf_mse_loss(output, target)
+  
+  if (i %% 200 == 0) {
+    print(as.matrix(output))
+    print(as.matrix(target))
+    print(loss$item())
+  }
+  
+  i <<- i + 1
+  
+  loss$backward()
+  optimizer$step()
+  
+  gc(full = TRUE)
+  
+  loss$item()
+  
+}
+
+valid_batch <- function(b) {
+  
+  output <- net(b$x$to(device = device))
+  target <- b$y$to(device = device)
+  
+  loss <-  nnf_mse_loss(output, target)
+  
+  gc(full = TRUE)
+  
+  loss$item()
+  
+}
+
+for (epoch in 1:num_epochs) {
+  
+  net$train()
+  train_loss <- c()
+  
+  i <- 1
+
+  for (b in enumerate(train_dl)) {
+    loss <- train_batch(b)
+    train_loss <- c(train_loss, loss)
+  }
+
+  torch_save(net, paste0("model_", epoch, ".pt"))
+
+  cat(sprintf("\nEpoch %d, training: loss:%3f\n", epoch, mean(train_loss)))
+  
+  net$eval()
+  valid_loss <- c()
+
+  for (b in enumerate(valid_dl)) {
+    
+    loss <- valid_batch(b)
+    valid_loss <- c(valid_loss, loss)
+    
+  }
+  
+  cat(sprintf("\nEpoch %d, validation: loss:%3f\n", epoch, mean(valid_loss)))
+}
+
 
 
 
 # https://holmdk.github.io/2020/04/02/video_prediction.html
-
 
 # https://arxiv.org/abs/1511.06432
 # Delving Deeper into Convolutional Networks for Learning Video Representations
